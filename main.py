afe79d56d84d89c82e93a642a0fd22270dff19fe
ghofar al amin <ghofaralamin2002@gmail.com>
2023-09-24 18:32:58 +0700

    update random

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 
 def alfabet(): #generate huruf alfabet a sampai z
     huruf = []
@@ -17,14 +18,15 @@ def main(hurufdpn,jumlah,kota): #fungsi utama, hurufdpn = huruf depan, jumlah = 
     doc = open(f"{kota}.txt","w") #membuat dokumen untuk output
     huruf_depan = hurufdpn
     hubel = alfabet()
-    indexhubel = 0 #indexing alfabet
+    rand_one = 0 #randomize
+    rand_two = 0
     angka_satu = 0
     angka_puluh = 0
     angka_ratus = 0
     angka_ribu = 0
     for i in range(0,jumlah):
-        doc.write(hurufdpn +" "+ str(angka_ribu + angka_ratus + angka_puluh + angka_satu) +" "+ str(hubel[indexhubel])+"\n") # membuka dan menulis dokumen yang sudah dibuat tadi
-        print(hurufdpn +" "+ str(angka_ribu + angka_ratus + angka_puluh + angka_satu) +" "+ str(hubel[indexhubel]))
+        doc.write(hurufdpn +" "+ str(angka_ribu + angka_ratus + angka_puluh + angka_satu) +" "+ str(hubel[rand_one])+str(hubel[rand_two])+"\n") # membuka dan menulis dokumen yang sudah dibuat tadi
+        print(hurufdpn +" "+ str(angka_ribu + angka_ratus + angka_puluh + angka_satu) +" "+ str(hubel[rand_one])+str(hubel[rand_two]))
         angka_satu += 1
         if angka_satu == 10:
             angka_puluh +=1
@@ -33,9 +35,8 @@ def main(hurufdpn,jumlah,kota): #fungsi utama, hurufdpn = huruf depan, jumlah = 
         elif angka_ratus == 10:
             angka_ribu +=1
 
-        indexhubel += 1
-        if indexhubel == 25:
-            indexhubel = 0
+        rand_one = random.randint(0,25)
+        rand_two = random.randint(0,25)
 
 
     doc.close() #menutup dokumen setelah kita buka
